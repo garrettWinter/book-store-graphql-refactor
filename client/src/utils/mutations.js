85d6823e48958b07ac191b4e17ed8bfaac1c7a98
Garrett Winter <Garrett_Winter@hotmail.com>
2023-04-03 11:00:04 -0500

    Continued to take first pass to flesh out remaining mutations.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,9 +12,39 @@ export const LOGIN_USER = gql`
   }
 `;
 
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const SAVE_BOOK = gql`
   mutation saveBook($bookDetails: saveBookVariables!) {
     saveBook(bookDetails: $bookDetails) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookDetails: saveBookVariables!) {
+    removeBook(bookDetails: $bookDetails) {
       _id
       username
       email
